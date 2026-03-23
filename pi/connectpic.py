907a0824550f781c44b1e86631048f46eb341720
Evan Noyes <noyesep@mail.uc.edu>
2026-03-23 18:25:20 -0400

    QR code

---
+++ pi/connectpic.py
@@ -0,0 +1,20 @@
+from flask import Flask, send_file
+import qrcode
+import io
+
+app = Flask(__name__)
+
+@app.route("/qr")
+def qr():
+    url = "http://192.168.1.84:3000/session.html"  # change later
+
+    img = qrcode.make(url)
+
+    buffer = io.BytesIO()
+    img.save(buffer, format="PNG")
+    buffer.seek(0)
+
+    return send_file(buffer, mimetype="image/png")
+
+if __name__ == "__main__":
+    app.run(debug=True)
